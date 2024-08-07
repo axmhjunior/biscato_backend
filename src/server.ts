405146da3bd2f29132babe2840a511c864c83378
AlexandreMahumane <alexandremahumane69@gmail.com>
2024-08-07 14:29:25 -0700

    update

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-
+import cors from 'cors'
 const app = express();
 require('dotenv').config()
 
@@ -9,6 +9,13 @@ const port = process.env.PORT || 8080
 
 app.use(express.json());
 
+app.use((request, response, next)=>{
+    response.header("Acess-Control-Allow-origin", "*");
+    response.header("Acess-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    response.header("Acess-Control-Allow-Headers", "Content-Type");
+
+    app.use(cors())
+})
 app.use('/user', require('./routes/User'));
 app.use('/adm', require('./routes/Adm'));
 app.use('/freelancer', require('./routes/Freelancer'));
